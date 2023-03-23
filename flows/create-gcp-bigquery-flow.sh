7249aa997b834b6ce1e67024a815406ee2124542
Matt Billenstein <matt@vazor.com>
2023-03-23 06:56:54 -0700

    Add bigquery flow script

---
+++ flows/create-gcp-bigquery-flow.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# This is just an example, fill in the vars as you need...
+
+# take auth from file - it's a bit awkward to cut and paste...
+AUTH="$(jq -c . gs-auth.json)"
+SCHEMA="$(jq -c . table-schema.json)"
+
+curl -s -u $APIKEY: -X POST 'https://hexcloud.co/v1/flow/create' \
+  -H 'Content-Type: application/json' \
+  -d @- <<EOF
+{
+  "name": "My bigquery Flow",
+  "sinks": [{
+    "type": "bigquery",
+    "dataset": "my_dataset",
+    "table": "my_table",
+    "schema": $SCHEMA,
+    "auth": $AUTH
+  }]
+}
+EOF
